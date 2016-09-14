e725f4bff2dd6f4cd59aef4ac5be5095f8dc2645
alex85vol <alex85vol@gmail.com>
2016-09-14 13:49:28 +0300

    no message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,9 @@ $("#main").append("Alex Vol");
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var  firstName = "alex";
+ var  age = 32;
+ console.log(firstName);
+
+ var  awesomeThoughts = "I am Alex and I am AWESOME!";
+ console.log(awesomeThoughts);
