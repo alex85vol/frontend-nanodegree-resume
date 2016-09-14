7bdeb232893c5d98d39b144a1cd93c7e31c993ac
alex85vol <alex85vol@gmail.com>
2016-09-14 14:09:19 +0300

    string replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,13 @@
-$("#main").append("Alex Vol");
+//$("#main").append("Alex Vol");
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- var  firstName = "alex";
- var  age = 32;
- console.log(firstName);
+// var  firstName = "alex";
+// var  age = 32;
+// console.log(firstName);
 
  var  awesomeThoughts = "I am Alex and I am AWESOME!";
  console.log(awesomeThoughts); 
+
+ var  funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ $('#main').append(funThoughts);
